45d4809cdf528024970dfeedae1788e8bcaaaee9
gary <gary.white@lodgit.com>
2022-06-21 13:19:13 +0200

    code working

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 function preFizz(n) {
-
+    let myArr = [];
+    for (var i = 1; i <= n; i++) {
+        myArr.push(i);
+    }
+    return myArr;
 }
 
 console.log(preFizz(1)); // [1]
-console.log(preFizz(2)); // [1, 2]
+console.log(preFizz(2)); // [1,2]
 console.log(preFizz(3)); // [1,2,3]
 console.log(preFizz(4)); // [[1,2,3,4]
 console.log(preFizz(5)); // [1,2,3,4,5]
