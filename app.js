725633a692211db71a44e84a7517b4b6dba8fe50
gary <gary.white@lodgit.com>
2022-06-21 13:14:21 +0200

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function preFizz(n) {
+
+}
+
+console.log(preFizz(1)); // [1]
+console.log(preFizz(2)); // [1, 2]
+console.log(preFizz(3)); // [1,2,3]
+console.log(preFizz(4)); // [[1,2,3,4]
+console.log(preFizz(5)); // [1,2,3,4,5]
